a84fa4d17ce3e234bd0f9b42e8035dfab1d580b7
Student <student@Mac-mini-31.local>
2015-09-08 12:45:15 -0600

    added bug variable

--- ang.js
+++ ang.js
@@ -1,6 +1,7 @@
 var app = angular.module("hithere", []);
-app.controller("HelloController", function HelloController($scope){
-	$scope.message = "Hello World!";
+app.controller("MainController", function MainController($scope){
+	$scope.message = "This should be in the console";
+	$scope.bug = "This should be in the html";
 	$scope.printthis = function() {
 		console.log($scope.message);
 		$scope.message_visible = true;
